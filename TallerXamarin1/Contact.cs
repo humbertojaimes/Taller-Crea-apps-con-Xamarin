7acb03e2ee85d0b119f348aca4059853344618cf
HJR <HJR@DESKTOP-41I1JEU>
2017-04-22 16:52:22 -0500

    Add project files.

---
+++ TallerXamarin1/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace TallerXamarin1
+{
+	public class Contact 
+	{
+
+		private string id;
+
+		public string ID
+		{
+			get { return id.Trim(); }
+			set { id = value.Trim();  }
+		}
+
+
+		private string fullName;
+
+		public string FullName
+		{
+			get { return fullName; }
+			set { fullName = value;  }
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value;  }
+		}
+
+
+		private string phoneNumber;
+
+		public string PhoneNumber
+		{
+			get { return phoneNumber; }
+			set { phoneNumber = value;  }
+		}
+
+	}
+}
---
+++ TallerXamarin1/TallerXamarin1Page.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TallerXamarin1
+{
+	public partial class TallerXamarin1Page : ContentPage
+	{
+		public TallerXamarin1Page()
+		{
+			InitializeComponent();
+			Button btn = new Button();
+
+		}
+
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			var contacts = await GetContacts();
+			lvContacts.ItemsSource = contacts;
+		}
+
+		const string uri = "http://adventureservicedemo.azurewebsites.net/Api/Contacts";
+
+		public async Task<List<Contact>> GetContacts()
+		{
+			List<Contact> result = null;
+
+			HttpRequestMessage getContactsRequest =
+				new HttpRequestMessage();
+			getContactsRequest.RequestUri = new Uri(uri);
+			getContactsRequest.Headers.Add("Accept", "application/json");
+			getContactsRequest.Method = HttpMethod.Get;
+
+			HttpClient client = new HttpClient();
+
+			HttpResponseMessage getContactsResponse =
+				 await client.SendAsync(getContactsRequest);
+			if (getContactsResponse.StatusCode == HttpStatusCode.OK)
+			{
+				result =
+					Newtonsoft.Json.JsonConvert.DeserializeObject<List<Contact>>
+							  (await getContactsResponse.Content.ReadAsStringAsync());
+			}
+
+			return result;
+		}
+	}
+}
